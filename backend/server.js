4eb5d976cd94c9ed6b47f307fc33168f7cd2b6e1
devanshu13052005 <152134089+devanshu13052005@users.noreply.github.com>
2026-03-26 00:09:22 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,35 @@ import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
+const allowedOrigins = [
+  "https://food-delivery-system-chi-silk.vercel.app",
+  "https://tomato-admin-devanshu.vercel.app"
+];
+
+
 // app config
 const app = express();
 const port =process.env.PORT || 4000;
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // DB connection
 connectDB();
